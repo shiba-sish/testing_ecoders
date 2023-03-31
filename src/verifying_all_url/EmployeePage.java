3483d92494ac4ed5d7b32cc767c0a0534677fdf2
Shibasish China <Shibasish China@SMTC-R-0157>
2023-03-31 13:18:35 +0530

    second commit

---
+++ src/verifying_all_url/EmployeePage.java
@@ -0,0 +1,31 @@
+package verifying_all_url;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EmployeePage {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Shibasish China\\eclipse-workspace\\testing_ecoders\\all_necessary_file/chromedriver.exe");
+		 WebDriver driver= new ChromeDriver();
+		 driver.get("http://127.0.0.1:5000/employee");
+		 Thread.sleep(2000);
+		 String title = driver.getTitle();
+		 System.out.println("title is->"+title);
+		 String curl = driver.getCurrentUrl();
+		 System.out.println("current url is-->"+curl);
+		 String ps = driver.getPageSource();
+		 System.out.println("page source-->"+ps);
+		 
+		 String extectedurl= "http://127.0.0.1:5000/employee";
+		 if(extectedurl.equals(curl)) {
+			 System.out.println("Matched test case pass!!!");
+		 }
+		 else {
+			 System.out.println("Not matched test case fail!!!");
+		 }
+		 
+		 driver.quit();
+	}
+
+}
